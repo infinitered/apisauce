082647998850a71c467f90c244c16c4e8fa05668
Steve Kellock <steve@kellock.ca>
2016-11-03 09:41:39 -0400

    Bumps all dependencies, adds yarn, and rebuilds the dist.

--- dist/apisauce.js
+++ dist/apisauce.js
@@ -8,6 +8,133 @@ var axios = _interopDefault(require('axios'));
 var R = _interopDefault(require('ramda'));
 var RS = _interopDefault(require('ramdasauce'));
 
+var asyncGenerator = function () {
+  function AwaitValue(value) {
+    this.value = value;
+  }
+
+  function AsyncGenerator(gen) {
+    var front, back;
+
+    function send(key, arg) {
+      return new Promise(function (resolve, reject) {
+        var request = {
+          key: key,
+          arg: arg,
+          resolve: resolve,
+          reject: reject,
+          next: null
+        };
+
+        if (back) {
+          back = back.next = request;
+        } else {
+          front = back = request;
+          resume(key, arg);
+        }
+      });
+    }
+
+    function resume(key, arg) {
+      try {
+        var result = gen[key](arg);
+        var value = result.value;
+
+        if (value instanceof AwaitValue) {
+          Promise.resolve(value.value).then(function (arg) {
+            resume("next", arg);
+          }, function (arg) {
+            resume("throw", arg);
+          });
+        } else {
+          settle(result.done ? "return" : "normal", result.value);
+        }
+      } catch (err) {
+        settle("throw", err);
+      }
+    }
+
+    function settle(type, value) {
+      switch (type) {
+        case "return":
+          front.resolve({
+            value: value,
+            done: true
+          });
+          break;
+
+        case "throw":
+          front.reject(value);
+          break;
+
+        default:
+          front.resolve({
+            value: value,
+            done: false
+          });
+          break;
+      }
+
+      front = front.next;
+
+      if (front) {
+        resume(front.key, front.arg);
+      } else {
+        back = null;
+      }
+    }
+
+    this._invoke = send;
+
+    if (typeof gen.return !== "function") {
+      this.return = undefined;
+    }
+  }
+
+  if (typeof Symbol === "function" && Symbol.asyncIterator) {
+    AsyncGenerator.prototype[Symbol.asyncIterator] = function () {
+      return this;
+    };
+  }
+
+  AsyncGenerator.prototype.next = function (arg) {
+    return this._invoke("next", arg);
+  };
+
+  AsyncGenerator.prototype.throw = function (arg) {
+    return this._invoke("throw", arg);
+  };
+
+  AsyncGenerator.prototype.return = function (arg) {
+    return this._invoke("return", arg);
+  };
+
+  return {
+    wrap: function (fn) {
+      return function () {
+        return new AsyncGenerator(fn.apply(this, arguments));
+      };
+    },
+    await: function (value) {
+      return new AwaitValue(value);
+    }
+  };
+}();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 var _extends = Object.assign || function (target) {
   for (var i = 1; i < arguments.length; i++) {
     var source = arguments[i];
@@ -20,6 +147,69 @@ var _extends = Object.assign || function (target) {
   }
 
   return target;
+};
+
+var get = function get(object, property, receiver) {
+  if (object === null) object = Function.prototype;
+  var desc = Object.getOwnPropertyDescriptor(object, property);
+
+  if (desc === undefined) {
+    var parent = Object.getPrototypeOf(object);
+
+    if (parent === null) {
+      return undefined;
+    } else {
+      return get(parent, property, receiver);
+    }
+  } else if ("value" in desc) {
+    return desc.value;
+  } else {
+    var getter = desc.get;
+
+    if (getter === undefined) {
+      return undefined;
+    }
+
+    return getter.call(receiver);
+  }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var set = function set(object, property, value, receiver) {
+  var desc = Object.getOwnPropertyDescriptor(object, property);
+
+  if (desc === undefined) {
+    var parent = Object.getPrototypeOf(object);
+
+    if (parent !== null) {
+      set(parent, property, value, receiver);
+    }
+  } else if ("value" in desc && desc.writable) {
+    desc.value = value;
+  } else {
+    var setter = desc.set;
+
+    if (setter !== undefined) {
+      setter.call(receiver, value);
+    }
+  }
+
+  return value;
 };
 
 // check for an invalid config
@@ -98,8 +288,8 @@ var create = function create(config) {
     Make the request for GET, HEAD, DELETE
    */
   var doRequestWithoutBody = function doRequestWithoutBody(method, url) {
-    var params = arguments.length <= 2 || arguments[2] === undefined ? {} : arguments[2];
-    var axiosConfig = arguments.length <= 3 || arguments[3] === undefined ? {} : arguments[3];
+    var params = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : {};
+    var axiosConfig = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : {};
 
     return doRequest(R.merge({ url: url, params: params, method: method }, axiosConfig));
   };
@@ -108,8 +298,8 @@ var create = function create(config) {
     Make the request for POST, PUT, PATCH
    */
   var doRequestWithBody = function doRequestWithBody(method, url) {
-    var data = arguments.length <= 2 || arguments[2] === undefined ? null : arguments[2];
-    var axiosConfig = arguments.length <= 3 || arguments[3] === undefined ? {} : arguments[3];
+    var data = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : null;
+    var axiosConfig = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : {};
 
     return doRequest(R.merge({ url: url, method: method, data: data }, axiosConfig));
   };
@@ -226,7 +416,9 @@ var create = function create(config) {
     head: R.partial(doRequestWithoutBody, ['head']),
     post: R.partial(doRequestWithBody, ['post']),
     put: R.partial(doRequestWithBody, ['put']),
-    patch: R.partial(doRequestWithBody, ['patch'])
+    patch: R.partial(doRequestWithBody, ['patch']),
+    link: R.partial(doRequestWithoutBody, ['link']),
+    unlink: R.partial(doRequestWithoutBody, ['unlink'])
   };
   // send back the sauce
   return sauce;
