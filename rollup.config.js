34f8c9bc888c9aaa46ac3e38a6e782c2bf6e469b
Steve Kellock <steve@kellock.ca>
2017-03-17 10:26:59 -0400

    Adds uglify to minify.

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,6 @@
 import babel from 'rollup-plugin-babel'
+import uglify from 'rollup-plugin-uglify'
+import filesize from 'rollup-plugin-filesize'
 
 export default {
   entry: 'lib/apisauce.js',
@@ -8,7 +10,9 @@ export default {
       babelrc: false,
       presets: ['es2015-rollup'],
       plugins: ['fast-async', 'transform-object-rest-spread']
-    })
+    }),
+    uglify(),
+    filesize()
   ],
   exports: 'named',
   dest: 'dist/apisauce.js',
