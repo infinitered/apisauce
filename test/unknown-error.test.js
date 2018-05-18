fea7bcb0cee08ffc19f3980bede476462ec06663
d.tupalov <d.tupalov@tyme.ru>
2018-05-18 14:47:06 +0300

    add new property 'originalError' for response (#147)
    * property originalError with original AxiosError
    
    * add test for adding property originalError
    
    * edit ts defs
    
    * add prettier config in package.json
    
    * add gitignore for ".vscode" dir

---
+++ test/unknown-error.test.js
@@ -0,0 +1,18 @@
+import test from 'ava'
+import { create, UNKNOWN_ERROR } from '../lib/apisauce'
+
+const impossiblePort = 65536
+
+test('add originalError for UNKNOWN_ERROR', t => {
+  const x = create({
+    baseURL: `http://localhost:${impossiblePort}`
+  })
+  return x.get('/').then(response => {
+    t.falsy(response.ok)
+    t.is(response.problem, UNKNOWN_ERROR)
+    t.is(
+      response.originalError.message,
+      '"port" option should be >= 0 and < 65536: 65536'
+    )
+  })
+})
