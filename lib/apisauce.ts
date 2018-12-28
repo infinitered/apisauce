81d1d033f72e9a3f76c2ad992976af19d67df1fa
Robin Heinze <robin.m.heinze@gmail.com>
2018-12-28 14:03:19 -0800

    fix(linting): Add prettier, tslint, and lint-staged/husky

--- lib/apisauce.ts
+++ lib/apisauce.ts
@@ -1,5 +1,4 @@
-import axios from 'axios'
-import { AxiosResponse, AxiosError } from 'axios'
+import axios, { AxiosResponse, AxiosError } from 'axios'
 import {
   cond,
   isNil,
@@ -18,7 +17,7 @@ import {
   pipe,
   partial,
   contains,
-  always
+  always,
 } from 'ramda'
 
 /**
@@ -33,11 +32,7 @@ import {
  * @example
  * toNumber('7') //=> 7
  */
-const toNumber = cond([
-  [isNil, identity],
-  [is(Number), identity],
-  [T, x => Number(x)]
-])
+const toNumber = cond([[isNil, identity], [is(Number), identity], [T, x => Number(x)]])
 
 /**
  * Given a min and max, determines if the value is included
@@ -58,13 +53,7 @@ const toNumber = cond([
  */
 const isWithin = curryN(3, (min, max, value) => {
   const isNumber = is(Number)
-  return (
-    isNumber(min) &&
-    isNumber(max) &&
-    isNumber(value) &&
-    gte(value, min) &&
-    gte(max, value)
-  )
+  return isNumber(min) && isNumber(max) && isNumber(value) && gte(value, min) && gte(max, value)
 })
 
 // a workaround to deal with __ not being available from the ramda types in typescript
@@ -74,19 +63,17 @@ const containsText = curryN(2, (textToSearch, list) => contains(list, textToSear
  * Are we dealing with a promise?
  */
 const isPromise = obj =>
-  !!obj &&
-  (typeof obj === 'object' || typeof obj === 'function') &&
-  typeof obj.then === 'function'
+  !!obj && (typeof obj === 'object' || typeof obj === 'function') && typeof obj.then === 'function'
 
 // the default headers given to axios
 export const DEFAULT_HEADERS = {
   Accept: 'application/json',
-  'Content-Type': 'application/json'
+  'Content-Type': 'application/json',
 }
 
 // the default configuration for axios, default headers will also be merged in
 const DEFAULT_CONFIG = {
-  timeout: 0
+  timeout: 0,
 }
 
 export const NONE = null
@@ -99,19 +86,15 @@ export const UNKNOWN_ERROR = 'UNKNOWN_ERROR'
 export const CANCEL_ERROR = 'CANCEL_ERROR'
 
 const TIMEOUT_ERROR_CODES = ['ECONNABORTED']
-const NODEJS_CONNECTION_ERROR_CODES = [
-  'ENOTFOUND',
-  'ECONNREFUSED',
-  'ECONNRESET'
-]
+const NODEJS_CONNECTION_ERROR_CODES = ['ENOTFOUND', 'ECONNREFUSED', 'ECONNRESET']
 const in200s = isWithin(200, 299)
 const in400s = isWithin(400, 499)
 const in500s = isWithin(500, 599)
 const statusNil = ifElse(isNil, always(undefined), prop('status'))
 
 /**
-  What's the problem for this axios response?
-  */
+ * What's the problem for this axios response?
+ */
 export const getProblemFromError = error => {
   // first check if the error message is Network Error (set by axios at 0.12) on platforms other than NodeJS.
   if (error.message === 'Network Error') return NETWORK_ERROR
@@ -123,7 +106,7 @@ export const getProblemFromError = error => {
     [isNil, () => getProblemFromStatus(statusNil(error.response))],
     [containsText(TIMEOUT_ERROR_CODES), always(TIMEOUT_ERROR)],
     [containsText(NODEJS_CONNECTION_ERROR_CODES), always(CONNECTION_ERROR)],
-    [T, always(UNKNOWN_ERROR)]
+    [T, always(UNKNOWN_ERROR)],
   ])(error.code)
 }
 
@@ -136,15 +119,14 @@ export const getProblemFromStatus = status => {
     [in200s, always(NONE)],
     [in400s, always(CLIENT_ERROR)],
     [in500s, always(SERVER_ERROR)],
-    [T, always(UNKNOWN_ERROR)]
+    [T, always(UNKNOWN_ERROR)],
   ])(status)
 }
 
 /**
-  Creates a instance of our API using the configuration.
+ * Creates a instance of our API using the configuration.
  */
 export const create = config => {
-
   // combine the user's defaults with ours
   const headers = merge(DEFAULT_HEADERS, config.headers || {})
   const combinedConfig = merge(DEFAULT_CONFIG, dissoc('headers', config))
@@ -162,8 +144,7 @@ export const create = config => {
   const responseTransforms = []
 
   const addRequestTransform = transform => requestTransforms.push(transform)
-  const addAsyncRequestTransform = transform =>
-    asyncRequestTransforms.push(transform)
+  const addAsyncRequestTransform = transform => asyncRequestTransforms.push(transform)
   const addResponseTransform = transform => responseTransforms.push(transform)
 
   // convenience for setting new request headers
@@ -200,26 +181,26 @@ export const create = config => {
   }
 
   /**
-    Make the request for GET, HEAD, DELETE
+   * Make the request for GET, HEAD, DELETE
    */
   const doRequestWithoutBody = (method, url, params = {}, axiosConfig = {}) => {
     return doRequest(merge({ url, params, method }, axiosConfig))
   }
 
   /**
-    Make the request for POST, PUT, PATCH
+   * Make the request for POST, PUT, PATCH
    */
   const doRequestWithBody = (method, url, data = null, axiosConfig = {}) => {
     return doRequest(merge({ url, method, data }, axiosConfig))
   }
 
   /**
-    Make the request with this config!
+   * Make the request with this config!
    */
   const doRequest = async axiosRequestConfig => {
     axiosRequestConfig.headers = {
       ...headers,
-      ...axiosRequestConfig.headers
+      ...axiosRequestConfig.headers,
     }
 
     // add the request transforms
@@ -245,7 +226,7 @@ export const create = config => {
     const chain = pipe(
       convertResponse(toNumber(new Date())),
       // partial(convertResponse, [toNumber(new Date())]),
-      runMonitors
+      runMonitors,
     )
 
     return instance
@@ -255,8 +236,8 @@ export const create = config => {
   }
 
   /**
-    Fires after we convert from axios' response into our response.  Exceptions
-    raised for each monitor will be ignored.
+   * Fires after we convert from axios' response into our response.  Exceptions
+   * raised for each monitor will be ignored.
    */
   const runMonitors = ourResponse => {
     monitors.forEach(monitor => {
@@ -270,47 +251,42 @@ export const create = config => {
   }
 
   /**
-    Converts an axios response/error into our response.
+   * Converts an axios response/error into our response.
    */
-  const convertResponse = curry(
-    (startedAt: number, axiosResult: AxiosResponse | AxiosError) => {
-      const end: number = toNumber(new Date())
-      const duration: number = end - startedAt
+  const convertResponse = curry((startedAt: number, axiosResult: AxiosResponse | AxiosError) => {
+    const end: number = toNumber(new Date())
+    const duration: number = end - startedAt
 
-      // new in Axios 0.13 -- some data could be buried 1 level now
-      const isError =
-        axiosResult instanceof Error || axios.isCancel(axiosResult)
-      const axiosResponse = axiosResult as AxiosResponse
-      const axiosError = axiosResult as AxiosError
-      const response = isError ? axiosError.response : axiosResponse
-      const status = (response && response.status) || null
-      const problem = isError
-        ? getProblemFromError(axiosResult)
-        : getProblemFromStatus(status)
-      const originalError = isError ? axiosError : null
-      const ok = in200s(status)
-      const config = axiosResult.config || null
-      const headers = (response && response.headers) || null
-      let data = (response && response.data) || null
+    // new in Axios 0.13 -- some data could be buried 1 level now
+    const isError = axiosResult instanceof Error || axios.isCancel(axiosResult)
+    const axiosResponse = axiosResult as AxiosResponse
+    const axiosError = axiosResult as AxiosError
+    const response = isError ? axiosError.response : axiosResponse
+    const status = (response && response.status) || null
+    const problem = isError ? getProblemFromError(axiosResult) : getProblemFromStatus(status)
+    const originalError = isError ? axiosError : null
+    const ok = in200s(status)
+    const config = axiosResult.config || null
+    const headers = (response && response.headers) || null
+    let data = (response && response.data) || null
 
-      // give an opportunity for anything to the response transforms to change stuff along the way
-      let transformedResponse = {
-        duration,
-        problem,
-        originalError,
-        ok,
-        status,
-        headers,
-        config,
-        data
-      }
-      if (responseTransforms.length > 0) {
-        forEach(transform => transform(transformedResponse), responseTransforms)
-      }
-
-      return transformedResponse
+    // give an opportunity for anything to the response transforms to change stuff along the way
+    let transformedResponse = {
+      duration,
+      problem,
+      originalError,
+      ok,
+      status,
+      headers,
+      config,
+      data,
     }
-  )
+    if (responseTransforms.length > 0) {
+      forEach(transform => transform(transformedResponse), responseTransforms)
+    }
+
+    return transformedResponse
+  })
 
   // create the base object
   const sauce = {
@@ -336,13 +312,13 @@ export const create = config => {
     put: partial(doRequestWithBody, ['put']),
     patch: partial(doRequestWithBody, ['patch']),
     link: partial(doRequestWithoutBody, ['link']),
-    unlink: partial(doRequestWithoutBody, ['unlink'])
+    unlink: partial(doRequestWithoutBody, ['unlink']),
   }
   // send back the sauce
   return sauce
 }
 
-export const { isCancel , CancelToken } = axios;
+export const { isCancel, CancelToken } = axios
 
 export default {
   DEFAULT_HEADERS,
@@ -355,5 +331,5 @@ export default {
   UNKNOWN_ERROR,
   create,
   isCancel,
-  CancelToken
+  CancelToken,
 }
